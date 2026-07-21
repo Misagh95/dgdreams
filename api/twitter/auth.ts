8dd628512f680c209f9417e9352997c4e144c6a8
Misagh95 <misaghmafi9@gmail.com>
2026-07-21 14:19:51 +0330

    Add tweet.read scope required by /2/users/me

--- api/twitter/auth.ts
+++ api/twitter/auth.ts
@@ -3,7 +3,7 @@ import crypto from 'crypto';
 
 const CLIENT_ID = process.env.TWITTER_CLIENT_ID || '';
 const CALLBACK_URL = (process.env.APP_URL || 'http://localhost:5173') + '/api/twitter/callback';
-const SCOPE = 'users.read';
+const SCOPE = 'tweet.read users.read';
 
 export default async function handler(_req: IncomingMessage, res: ServerResponse) {
   const state = crypto.randomBytes(16).toString('hex');
